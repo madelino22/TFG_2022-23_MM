9cd6d5a2bd5aaf36af992e627ad794a78e6fdf97
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2022-09-08 16:05:29 +0200

    Sprites in menus

--- TFGMM/Assets/Scripts/LoadMenuInfo.cs
+++ TFGMM/Assets/Scripts/LoadMenuInfo.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class LoadMenuInfo : MonoBehaviour
 {
     public TextMeshProUGUI topLeftName;
 
-    public TextMeshProUGUI midButtonName;
+    public Text midButtonName;
 
     public TextMeshProUGUI roomCode;
 
